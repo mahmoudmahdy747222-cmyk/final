b3758df072fa8c78b5380bd99fbc7e209540ef08
omrm6338-stack <omrm6338@gmail.com>
2026-05-12 11:03:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,31 @@
-function search() {/*for search*/
-  let input = document.querySelector(".searching").value.toLowerCase();
+console.log("hello");
+let button=document.querySelector('button');
+let nav=document.querySelector('nav');
+let footer=document.querySelector('footer');
+let head=document.querySelector('h1');
+let categories=document.querySelector('.Categories');
+let head3=document.
+button.onclick =function(){
+  if(document.body.style.backgroundColor==='black'){//for the main mood
+  document.body.style.backgroundColor='white';
+  nav.style.backgroundColor='#402009';
+  nav.style.boxShadow='0px 3px 4px 3px #7f4412';
+  button.style.boxShadow='0px 3px 4px 3px #7f4412';
+  footer.style.backgroundColor='#402009';
+  head.style.color='#402009';
+  head.style.textShadow='0px 3px 4px #7f4412';
+  categories.style.backgroundColor='white';
+  }
+  else{//for dark mode. 
+  document.body.style.backgroundColor='black';
+  nav.style.backgroundColor='black';
+  button.style.boxShadow='0px 0px 10px -1px white';
+  nav.style.boxShadow='0px 0px 10px -1px white';
+  nav.style.color='white';
+  footer.style.backgroundColor=('#141414');
+  head.style.color=('white');
+  head.style.textShadow='2px 2px 5px white';
+  categories.style.backgroundColor='black';
   
-  let sections = document.querySelectorAll('.fantasy, .history, .psychology, .best-seller');
-
-  sections.forEach(section => {
-    let cards = section.querySelectorAll(".book-card");
-    let hasVisibleCards = false;
-
-    cards.forEach(card => {
-      let title = card.querySelector(".photo-info").textContent.toLowerCase();
-      
-      if (title.includes(input)) {
-        card.style.display = ""; 
-        hasVisibleCards = true; 
-      } else {
-        card.style.display = "none";
-      }
-    });
-
-    let header = section.previousElementSibling; 
-    
-    if (hasVisibleCards) {
-      section.style.display = "grid";
-      if (header) header.style.display = "flex";
-    } else {
-      section.style.display = "none";
-      if (header) header.style.display = "none";
-    }
-  });
-}
-
-let themeBtn = document.getElementById("theme-chnger");/*for theme8*/
-
-if (localStorage.getItem("mode") === "dark") {
-  document.body.classList.add("dark");
-}
-
-
-themeBtn.onclick = function() {
-  document.body.classList.toggle("dark");
-
-  if (document.body.classList.contains("dark")) {
-    localStorage.setItem("mode", "dark");
-  } else {
-    localStorage.setItem("mode", "light");
   }
 };
